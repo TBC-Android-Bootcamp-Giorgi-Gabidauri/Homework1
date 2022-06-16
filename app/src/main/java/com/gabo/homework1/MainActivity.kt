6dfd21fae79a55e43f28ac7ee7014caf1380869a
Giorgi Gabidauri <giorgi.gabidauri16@gmail.com>
2022-06-17 01:42:17 +0400

    improved errorHandling

--- app/src/main/java/com/gabo/homework1/MainActivity.kt
+++ app/src/main/java/com/gabo/homework1/MainActivity.kt
@@ -14,7 +14,7 @@ fun main() {
     start()
 }
 
-fun takeX(): Int? {
+private fun takeX(): Int? {
     println("შეიყვანეთ X ცვლადის მნიშვნელობა")
     val x = readLine()?.toIntOrNull()
     if (x == null) {
@@ -23,7 +23,7 @@ fun takeX(): Int? {
     return x
 }
 
-fun takeY(x: Int?): Int? {
+private fun takeY(x: Int?): Int? {
     println("შეიყვანეთ Y ცვლადის მნიშვნელობა")
     val y = readLine()?.toIntOrNull()
     if (y == null) {
@@ -32,7 +32,7 @@ fun takeY(x: Int?): Int? {
     return y
 }
 
-fun sum(x: Int?, y: Int?) {
+private fun sum(x: Int?, y: Int?) {
     if (x is Int && y is Int) {
         val sum = x + y
         println("X და Y - ის ჯამი არის $sum")
@@ -49,7 +49,7 @@ fun sum(x: Int?, y: Int?) {
     }
 }
 
-fun start() {
+private fun start() {
     val x = takeX()
     if (x is Int) {
         val y = takeY(x)
@@ -59,7 +59,7 @@ fun start() {
     }
 }
 
-fun error() {
+private fun error() {
     println(
         "თქვენს ჩანაწერში დაფიქსირდა შეცდომა,გთხოვთ მიუთითოთ Y ან N \n გსურთ პროგრამის ხელახლა დაწყება? \n" +
                 " <Y/N>?"
@@ -73,7 +73,7 @@ fun error() {
     }
 }
 
-fun errorX() {
+private fun errorX() {
     println(
         "თქვენს ჩანაწერში დაფიქსირდა შეცდომა, გთხოვთ შეიყვანოთ რიცხვები \n გსურთ პროგრამის ხელახლა დაწყება?\n <Y/N>?"
     )
@@ -86,7 +86,7 @@ fun errorX() {
     }
 }
 
-fun errorY(x: Int?) {
+private fun errorY(x: Int?) {
     println("თქვენს ჩანაწერში დაფიქსირდა შეცდომა, გთხოვთ შეიყვანოთ რიცხვები \n გსურთ სცადოთ ახლიდან? \n <Y/N>?")
     when (readLine()) {
         "y" -> {
@@ -103,7 +103,7 @@ fun errorY(x: Int?) {
     }
 }
 
-fun errorResume(x: Int?) {
+private fun errorResume(x: Int?) {
     println("თქვენს ჩანაწერში დაფიქსირდა შეცდომა, გთხოვთ მიუთითოთ Y ან N \n გსურთ გაგრძელება? \n <Y/N>?")
     when (readLine()) {
         "y" -> {
